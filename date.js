a8755288578670a57675f54388d5750f31511bae
Balint Fulop <bfulop@mac.com>
2019-02-14 18:35:04 +0100

    finally working on Now

--- date.js
+++ date.js
@@ -3,26 +3,27 @@ import mjml2html from 'mjml'
 /*
   Compile an mjml string
 */
-// const renderMJML = () => mjml2html(`
-//   <mjml>
-//     <mj-body>
-//       <mj-section>
-//         <mj-column>
-//           <mj-text>
-//             Hello World!
-//           </mj-text>
-//         </mj-column>
-//       </mj-section>
-//     </mj-body>
-//   </mjml>
-// `, {})
+const renderMJML = () => mjml2html(`
+  <mjml>
+    <mj-body>
+      <mj-section>
+        <mj-column>
+          <mj-text>
+            Hello World!
+          </mj-text>
+        </mj-column>
+      </mj-section>
+    </mj-body>
+  </mjml>
+`, {})
 
 
 /*
   Print the responsive HTML generated and MJML errors if any
 */
-// console.log(renderMJML())
+console.log(renderMJML())
 module.exports = (req, res) => {
+    const result = renderMJML()
     // console.log(renderMJML())
-    res.end('done rendering')
+    res.end(result.html)
 }
